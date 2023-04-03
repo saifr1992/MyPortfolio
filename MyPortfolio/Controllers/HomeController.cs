1d920eb8c2809c3e7c0edc70b4b7debed458a477
Saif Rehman <saif@devhouse.co>
2023-04-03 14:38:39 +0500

    init

---
+++ MyPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Models;
+
+namespace MyPortfolio.Controllers;
+
+[Route("")]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("", Name="Home-index")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet("About", Name = "Home-About")]
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
